f061841c0fbb7ced0d2fe7207cef6ceea505f415
unknown <snswaroop@.corp.ebay.com>
2014-08-02 12:33:36 +0530

    two constraint

--- src/Scrabble/Scrabble/src/com/scrabble/ScrabbleGame.java
+++ src/Scrabble/Scrabble/src/com/scrabble/ScrabbleGame.java
@@ -1,178 +1,237 @@
-package com.scrabble;
+package scrabble;
 
 import java.io.*;
 import java.util.*;
 
 public class ScrabbleGame {
 
-	private static String constraintLetter;
+    public static String lcs(String a, String b) {
 
-	public static String lcs(String a, String b) {
+        int[][] lengths = new int[a.length() + 1][b.length() + 1];
 
-		int[][] lengths = new int[a.length() + 1][b.length() + 1];
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    lengths[i + 1][j + 1] = lengths[i][j] + 1;
+                } else {
+                    lengths[i + 1][j + 1] = Math.max(lengths[i + 1][j],
+                            lengths[i][j + 1]);
+                }
+            }
+        }
 
-		for (int i = 0; i < a.length(); i++)
-			for (int j = 0; j < b.length(); j++)
-				if (a.charAt(i) == b.charAt(j))
-					lengths[i + 1][j + 1] = lengths[i][j] + 1;
-				else
-					lengths[i + 1][j + 1] = Math.max(lengths[i + 1][j],
-							lengths[i][j + 1]);
+        StringBuffer sb = new StringBuffer();
 
-		StringBuffer sb = new StringBuffer();
+        for (int x = a.length(), y = b.length();
+                x != 0 && y != 0;) {
 
-		for (int x = a.length(), y = b.length();
+            if (lengths[x][y] == lengths[x - 1][y]) {
+                x--;
+            } else if (lengths[x][y] == lengths[x][y - 1]) {
+                y--;
+            } else {
+                assert a.charAt(x - 1) == b.charAt(y - 1);
 
-		x != 0 && y != 0;) {
+                sb.append(a.charAt(x - 1));
 
-			if (lengths[x][y] == lengths[x - 1][y])
+                x--;
+                y--;
+            }
+        }
 
-				x--;
-			else if (lengths[x][y] == lengths[x][y - 1])
+        return sb.reverse().toString();
 
-				y--;
-			else {
-				assert a.charAt(x - 1) == b.charAt(y - 1);
+    }
 
-				sb.append(a.charAt(x - 1));
+    static List<String> readDictionary(String fileName)
+            throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileReader(fileName));
+        List<String> dictionary = new ArrayList<String>();
 
-				x--;
-				y--;
-			}
-		}
+        while (sc.hasNextLine()) {
+            String word = sc.nextLine();
+            dictionary.add(word);
+        }
+        return dictionary;
+    }
 
-		return sb.reverse().toString();
+    public static boolean rules(String word, int numLeftSpaces, int numRightSpaces, String constraintLetter, String bconstraintLetter, int betweenSpace) {
 
-	}
+        boolean result = false;
+        
+        if (word.contains(constraintLetter)) {
+            if (word.indexOf(constraintLetter) <= numLeftSpaces) {
+                int endLength = bconstraintLetter.length()>0?betweenSpace+1:0;
+                if ((word.length() - word.indexOf(constraintLetter) - 1) <= numRightSpaces + endLength ) {
+                    
+                    if(bconstraintLetter.length() > 0){
+                        if ( (word.length() - word.indexOf(constraintLetter) - 1) >= betweenSpace+1 )
+                            result = true;
+                    }
+                    else
+                        result = true;
+                }
+            }
+        }
+        if (bconstraintLetter.length() > 0 && result) {
+           
+           result = false;
+            if (word.indexOf(bconstraintLetter, word.indexOf(constraintLetter) + 1) == word.indexOf(constraintLetter) + 1 + betweenSpace ) {
+                    if ((word.length() - word.indexOf(bconstraintLetter) - 1) <= numRightSpaces) {
+                        result = true;
+                    }
+                }
+            }
+           
+        
+        return result;
+    }
 
-	static List<String> readDictionary(String fileName)
-			throws FileNotFoundException {
-		Scanner sc = new Scanner(new FileReader(fileName));
-		List<String> dictionary = new ArrayList<String>();
+    static String searchInDictionary(String inputString, List<String> dictionary, int numLeftSpaces, int numRightSpaces, String constraintLetter, String bconstraintLetter, int betweenSpace) {
+        String maxWord = "";
+        int score = 0;
+        //System.out.println("inside func:"+numRightSpaces);
 
-		while (sc.hasNextLine()) {
-			String word = sc.nextLine();
-			dictionary.add(word);
-		}
-		return dictionary;
-	}
-	
-	public  static  boolean  rules(String word, int numLeftSpaces, int numRightSpaces){
-		
-		boolean result = false;
-		if(word.contains(constraintLetter))
-			if(word.indexOf(constraintLetter) <= numLeftSpaces )
-				if((word.length() - word.indexOf(constraintLetter) -1) <= numRightSpaces)
-					result = true;
-		return  result;
-	}
+        int max = 0;
+        //System.out.println(inputString.length());
+        for (int i = 0; i < dictionary.size(); i++) {
+            String dictionaryWord = dictionary.get(i);
 
-	static String searchInDictionary(String inputString, List<String> dictionary, int numLeftSpaces, int numRightSpaces) {
-		String maxWord = "";
-		int score = 0;
-		String dictionaryWord, prev = "";
-		//System.out.println("inside func:"+numRightSpaces);
+            char letterArray[] = dictionaryWord.toCharArray();
+            Arrays.sort(letterArray);
+            String sortedDictionaryWord = new String(letterArray);
 
-		int max = Integer.MIN_VALUE;
-		//System.out.println(inputString.length());
-		for (int i = 0; i < dictionary.size(); i++) {
-			dictionaryWord = dictionary.get(i);
+            int numOfextraLetters = constraintLetter.length() + bconstraintLetter.length();
+            if (inputString.length() == 7 + numOfextraLetters) {
+                String matchedString = lcs(inputString, sortedDictionaryWord);
+                if (matchedString.length() == sortedDictionaryWord.length()) {
+                    if (rules(dictionaryWord, numLeftSpaces, numRightSpaces, constraintLetter, bconstraintLetter, betweenSpace)) {
+                        score = calculateScore(matchedString);
+                    }
 
-			char letterArray[] = dictionaryWord.toCharArray();
-			Arrays.sort(letterArray);
-			String newDictionaryWord = new String(letterArray);
+                }
+            } else {
 
-			if (inputString.length() == 8 || inputString.length() == 7 ) {
-				  String matchedString = lcs(inputString, newDictionaryWord);
-                  if (matchedString.length() == newDictionaryWord.length()) {
-                	  if(rules(dictionaryWord,numLeftSpaces,numRightSpaces))
-                		  score = calculateScore(matchedString);
-                      
-                  }
-			} else {
+                String matchedString = lcs(inputString, sortedDictionaryWord);
+                if (matchedString.length() == sortedDictionaryWord.length() || matchedString.length() == sortedDictionaryWord.length() - 1) {
+                    if (rules(dictionaryWord, numLeftSpaces, numRightSpaces, constraintLetter,  bconstraintLetter,  betweenSpace)) {
+                        score = calculateScore(matchedString);
+                    }
+                }
 
-				String matchedString = lcs(inputString, newDictionaryWord);
-				if (matchedString.length() == newDictionaryWord.length()|| matchedString.length() == newDictionaryWord.length() - 1) {
-					if(rules(dictionaryWord,numLeftSpaces,numRightSpaces))
-						score = calculateScore(matchedString);
-				}
+            }
 
-			}
+            if (score > max ) {
+                maxWord = dictionaryWord;
+                max = score;
+            }
 
-			if (score > max) {
-				maxWord = dictionaryWord;
-				max = score;
-				prev = dictionaryWord;
-			}
+        }
 
-		}
-	
-		
-		return new String(prev+","+max);
-	}
+        return new String(maxWord + "," + max);
+    }
 
-	static int calculateScore(String word) {
-		int sum = 0;
-		int score[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1,
-				1, 1, 1, 4, 4, 8, 4, 10 };
+    static int calculateScore(String word) {
+        int sum = 0;
+        int score[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1,
+            1, 1, 1, 4, 4, 8, 4, 10};
 
-		for (int i = 0; i < word.length(); i++) {
-			sum += score[word.charAt(i) - 'A'];
-		}
-		return sum;
-	}
+        for (int i = 0; i < word.length(); i++) {
+            sum += score[word.charAt(i) - 'A'];
+        }
+        return sum;
+    }
 
-	public static void printResult(String input, String result){
-		
-		String[] splitString = result.split(",");
-		System.out.println(input+" "+splitString[0]+" "+splitString[1] );
-		
-		
-	}
-	public static void main(String[] args) throws FileNotFoundException {
+    public static void printResult( String result) {
 
-		List<String> dictionary = new ArrayList<String>();
-		String fileName = "sowpods.txt";
-		dictionary = readDictionary(fileName);
-		Scanner sc = new Scanner(new FileReader("input.txt"));
-		String constraint = new String("");
-		String scrambledWord = new String("");
-		int numLeftSpaces;
-		int numRightSpaces;
-		while (sc.hasNextLine()) {
-			constraint = new String("");
-			String[] input = sc.nextLine().split(" ");
-			if( input.length > 1){
-				constraint = input[0];
-				scrambledWord = input[1];
-				numLeftSpaces = getNumLeftSpaces(constraint);
-				numRightSpaces = getNumRightSpaces(constraint);
-			}
-			else{
-				scrambledWord = input[0];
-				numLeftSpaces = Integer.MAX_VALUE;
-				numRightSpaces = Integer.MAX_VALUE;
-				
-			}
-			//System.out.println(numRightSpaces);
-			scrambledWord += constraint.replace("_", "");
-			constraintLetter = constraint.replace("_", "");
-			char letterArray[] = scrambledWord.toUpperCase().toCharArray();
-			Arrays.sort(letterArray);
-			String inputString = new String(letterArray);
-			String result = searchInDictionary(inputString, dictionary,numLeftSpaces,numRightSpaces);
-			printResult(scrambledWord,result);
-		}
-	}
+        String[] splitString = result.split(",");
+        System.out.println( splitString[0] + " " + splitString[1]);
 
-	private static int getNumRightSpaces(String constraint) {
-		String constraintLetter = constraint.replace("_", "");
-		return constraint.length() - constraint.indexOf(constraintLetter) -1;
-	}
+    }
 
-	private static int getNumLeftSpaces(String constraint) {
-		String constraintLetter = constraint.replace("_", "");
-		return constraint.indexOf(constraintLetter);
-		 
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+
+        List<String> dictionary = new ArrayList<String>();
+        String fileName = "sowpods.txt";
+        dictionary = readDictionary(fileName);
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+      
+        while (sc.hasNextLine()) {
+              String constraint = new String("");
+        String scrambledWord = new String("");
+        int numLeftSpaces;
+        int numRightSpaces;
+        int betweenSpaces = 0;
+        String aconstraintLetter = "";
+        String bconstraintLetter = "";
+            constraint = new String("");
+            String[] input = sc.nextLine().split(" ");
+            if (input.length > 1) {
+                constraint = input[0];
+                scrambledWord = input[1];
+                String processedString = processConstraintString(constraint);
+                String[] splitConstraintString = processedString.split(" ");
+
+                numLeftSpaces = Integer.parseInt(splitConstraintString[0]);
+                aconstraintLetter = splitConstraintString[1].toUpperCase();
+
+                if (splitConstraintString.length > 3) {
+                    bconstraintLetter = splitConstraintString[3].toUpperCase();
+                    betweenSpaces = Integer.parseInt(splitConstraintString[2]);
+                    numRightSpaces = Integer.parseInt(splitConstraintString[4]);
+                } else {
+                    numRightSpaces = Integer.parseInt(splitConstraintString[2]);
+                }
+            } else {
+                scrambledWord = input[0];
+                numLeftSpaces = Integer.MAX_VALUE;
+                numRightSpaces = Integer.MAX_VALUE;
+
+            }
+            //System.out.println(numRightSpaces);
+            scrambledWord += constraint.replace("_", "");
+            char letterArray[] = scrambledWord.toUpperCase().toCharArray();
+            Arrays.sort(letterArray);
+            String inputString = new String(letterArray);
+            //System.out.println(inputString+" "+aconstraintLetter+" "+bconstraintLetter+" "+numLeftSpaces+ " "+ betweenSpaces + " "+ numRightSpaces);
+            String result = searchInDictionary(inputString, dictionary, numLeftSpaces, numRightSpaces, aconstraintLetter, bconstraintLetter, betweenSpaces);
+            printResult(result);
+        }
+    }
+
+    private static String processConstraintString(String constraint) {
+        String constraintLetter = constraint.replace("_", "");
+        int leftSpace = 0;
+        int rightSpace = 0;
+        int betweenSpace = 0;
+        String aConstraint, bConstraint;
+        int i = 0;
+        while (i < constraint.length() && constraint.charAt(i) == '_') {
+            leftSpace++;
+            i++;
+        }
+        aConstraint = constraint.substring(i, i+1 );
+        i++;
+        while (i < constraint.length() && constraint.charAt(i) == '_') {
+            betweenSpace++;
+            i++;
+        }
+        if (i < constraint.length()) {
+            bConstraint = constraint.substring(i, i+1 );
+            i++;
+
+            while (i < constraint.length() && constraint.charAt(i) == '_') {
+                rightSpace++;
+                i++;
+            }
+        } else {
+            rightSpace = betweenSpace;
+            String processedConstraint = leftSpace + " " + aConstraint + " " + rightSpace;
+            return processedConstraint;
+        }
+
+        String processedConstraint = leftSpace + " " + aConstraint + " " + betweenSpace + " " + bConstraint + " " + rightSpace;
+        return processedConstraint;
+    }
+
 }
